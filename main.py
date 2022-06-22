19ad62b03cf1906ac4a7070329d8aa3f3d9595f3
EddieFernandezSuit <83356892+EddieFernandezSuit@users.noreply.github.com>
2022-06-22 14:53:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -139,74 +139,74 @@ class Game:
         self.clock = pygame.time.Clock()
         self.fontSize = 25
         self.myFont = pygame.font.SysFont("freesansbold", self.fontSize)
-        self.start(self)
+        self.start()
         while True:
-            self.update(self)
             self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
             self.screen.fill(Colors.GREY)
-            self.draw(self)
             pygame.display.update()
             self.clock.tick(60)
+            self.update()
+            self.draw()
 
-    def start(game):
-        game.health1 = 20
-        game.health2 = 20
-        game.deleteCards = []
-        game.phase = 'play'
-        game.passTurnButton = PassTurnButton(game)
-        game.turn = 0
-        game.totalMana1 = 1
-        game.totalMana2 = 0
-        game.mana1 = 1
-        game.mana2 = 0
-        game.isSelected = 0
-        game.deck1 = []
-        game.deck2 = []
+    def start(self):
+        self.health1 = 20
+        self.health2 = 20
+        self.deleteCards = []
+        self.phase = 'play'
+        self.passTurnButton = PassTurnButton(self)
+        self.turn = 0
+        self.totalMana1 = 1
+        self.totalMana2 = 0
+        self.mana1 = 1
+        self.mana2 = 0
+        self.isSelected = 0
+        self.deck1 = []
+        self.deck2 = []
         for x in range(10):
-            game.deck1.append(Card('TGuy1', 1, 1, 1, 0))
+            self.deck1.append(Card('TGuy1', 1, 1, 1, 0))
         for x in range(10):
-            game.deck1.append(Card('TGuy2', 2, 2, 2,0))
+            self.deck1.append(Card('TGuy2', 2, 2, 2,0))
         for x in range(10):
-            game.deck1.append(Card('TGuy3', 3, 3, 3,0))
+            self.deck1.append(Card('TGuy3', 3, 3, 3,0))
         for x in range(10):
-            game.deck2.append(Card('TGuy1', 1, 1, 1,1))
+            self.deck2.append(Card('TGuy1', 1, 1, 1,1))
         for x in range(10):
-            game.deck2.append(Card('TGuy2', 2, 2, 2,1))
+            self.deck2.append(Card('TGuy2', 2, 2, 2,1))
         for x in range(10):
-            game.deck2.append(Card('TGuy3', 3, 3, 3,1))
-        random.shuffle(game.deck1)
-        random.shuffle(game.deck2)
+            self.deck2.append(Card('TGuy3', 3, 3, 3,1))
+        random.shuffle(self.deck1)
+        random.shuffle(self.deck2)
 
-        game.hand1 = []
-        game.hand2 = []
+        self.hand1 = []
+        self.hand2 = []
 
         for i in range(5):
-            game.deck1[-1].x = 5 + 205 * (i + 1)
-            game.hand1.append(game.deck1[-1])
-            game.deck1.pop(-1)
+            self.deck1[-1].x = 5 + 205 * (i + 1)
+            self.hand1.append(self.deck1[-1])
+            self.deck1.pop(-1)
 
         for i in range(5):
-            game.deck2[-1].x = 5 + 205 * (i + 1)
-            game.deck2[-1].y = game.SCREEN_HEIGHT - 205
-            game.hand2.append(game.deck2[-1])
-            game.deck2.pop(-1)
+            self.deck2[-1].x = 5 + 205 * (i + 1)
+            self.deck2[-1].y = self.SCREEN_HEIGHT - 205
+            self.hand2.append(self.deck2[-1])
+            self.deck2.pop(-1)
 
-        for x in range(len(game.deck1)):
-            print(game.deck1[x].name)
+        for x in range(len(self.deck1)):
+            print(self.deck1[x].name)
 
-        game.cards = []
-        for x in range(len(game.hand1)):
-            game.cards.append(game.hand1[x])
-        for i in range(len(game.hand2)):
-            game.cards.append(game.hand2[i])
+        self.cards = []
+        for x in range(len(self.hand1)):
+            self.cards.append(self.hand1[x])
+        for i in range(len(self.hand2)):
+            self.cards.append(self.hand2[i])
 
-        game.emptyZones = []
+        self.emptyZones = []
         for i in range(5):
-            game.emptyZones.append(EmptyZone(5 + (i+1) * 205, 210,0))
+            self.emptyZones.append(EmptyZone(5 + (i+1) * 205, 210,0))
         for i in range(5):
-            game.emptyZones.append(EmptyZone(5 + (i+1) * 205, game.SCREEN_HEIGHT - 410,1))
+            self.emptyZones.append(EmptyZone(5 + (i+1) * 205, self.SCREEN_HEIGHT - 410,1))
 
-    def update(game):
+    def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -214,71 +214,71 @@ class Game:
                 if event.key == pygame.K_SPACE:
                     a = 1
 
-        game.passTurnButton.update(game)
-        for i in range(len(game.cards)):
-            game.cards[i].update(game)
+        self.passTurnButton.update(self)
+        for i in range(len(self.cards)):
+            self.cards[i].update(self)
 
-        for i in range(len(game.hand1)):
-            game.hand1[i].x = 5 + 205 * (i + 1)
+        for i in range(len(self.hand1)):
+            self.hand1[i].x = 5 + 205 * (i + 1)
 
-        for i in range(len(game.hand2)):
-            game.hand2[i].x = 5 + 205 * (i + 1)
-            game.hand2[i].y = game.SCREEN_HEIGHT - 205
+        for i in range(len(self.hand2)):
+            self.hand2[i].x = 5 + 205 * (i + 1)
+            self.hand2[i].y = self.SCREEN_HEIGHT - 205
 
-        if game.isSelected == 1 and game.phase == 'play':
-            for i in range(len(game.emptyZones)):
+        if self.isSelected == 1 and self.phase == 'play':
+            for i in range(len(self.emptyZones)):
 
-                if pygame.mouse.get_pressed()[0] and game.emptyZones[i].rect.collidepoint(pygame.mouse.get_pos()) and game.emptyZones[i].isFull == 0:
-                    for j in range(len(game.cards)):
-                        if game.cards[j].isSelected == 1 and game.emptyZones[i].side == game.cards[j].side and game.phase == 'play' and game.cards[j].place == 'hand':
-                            game.cards[j].place = 'field'
-                            game.cards[j].x = game.emptyZones[i].x
-                            game.cards[j].y = game.emptyZones[i].y
-                            game.cards[j].isSelected = 0
-                            if game.cards[j].side == 0:
-                                for k in range(len(game.hand1)):
-                                    if game.hand1[k] == game.cards[j]:
-                                        game.hand1.pop(k)
+                if pygame.mouse.get_pressed()[0] and self.emptyZones[i].rect.collidepoint(pygame.mouse.get_pos()) and self.emptyZones[i].isFull == 0:
+                    for j in range(len(self.cards)):
+                        if self.cards[j].isSelected == 1 and self.emptyZones[i].side == self.cards[j].side and self.phase == 'play' and self.cards[j].place == 'hand':
+                            self.cards[j].place = 'field'
+                            self.cards[j].x = self.emptyZones[i].x
+                            self.cards[j].y = self.emptyZones[i].y
+                            self.cards[j].isSelected = 0
+                            if self.cards[j].side == 0:
+                                for k in range(len(self.hand1)):
+                                    if self.hand1[k] == self.cards[j]:
+                                        self.hand1.pop(k)
                                         break
                             else:
-                                for k in range(len(game.hand2)):
-                                    if game.hand2[k] == game.cards[j]:
-                                        game.hand2.pop(k)
+                                for k in range(len(self.hand2)):
+                                    if self.hand2[k] == self.cards[j]:
+                                        self.hand2.pop(k)
                                         break
-                            game.isSelected = 0
-                            game.emptyZones[i].isFull = 1
-                            if game.cards[j].side == 0:
-                                game.mana1 -= game.cards[j].mana
+                            self.isSelected = 0
+                            self.emptyZones[i].isFull = 1
+                            if self.cards[j].side == 0:
+                                self.mana1 -= self.cards[j].mana
                             else:
-                                game.mana2 -= game.cards[j].mana
+                                self.mana2 -= self.cards[j].mana
 
 
-        for i in reversed(range(len(game.deleteCards))):
+        for i in reversed(range(len(self.deleteCards))):
             print(i)
-            game.cards.pop(game.deleteCards[i])
-            game.deleteCards.pop(i)
+            self.cards.pop(self.deleteCards[i])
+            self.deleteCards.pop(i)
 
-    def draw(game):
-        for x in range(len(game.emptyZones)):
-            game.screen.blit(game.emptyZones[x].image, (game.emptyZones[x].x, game.emptyZones[x].y))
+    def draw(self):
+        for x in range(len(self.emptyZones)):
+            self.screen.blit(self.emptyZones[x].image, (self.emptyZones[x].x, self.emptyZones[x].y))
 
-        for x in range(len(game.cards)):
-            game.screen.blit(game.cards[x].image, (game.cards[x].x, game.cards[x].y))
-            temp = [str(game.cards[x].name), 'M: ' + str(game.cards[x].mana), 'D: ' + str(game.cards[x].damage),
-                    'H: ' + str(game.cards[x].health)]
+        for x in range(len(self.cards)):
+            self.screen.blit(self.cards[x].image, (self.cards[x].x, self.cards[x].y))
+            temp = [str(self.cards[x].name), 'M: ' + str(self.cards[x].mana), 'D: ' + str(self.cards[x].damage),
+                    'H: ' + str(self.cards[x].health)]
             for y in range(len(temp)):
-                drawOutlineText(game, temp[y] ,game.cards[x].x + 5, game.cards[x].y + 5 + y * game.fontSize)
+                drawOutlineText(self, temp[y] ,self.cards[x].x + 5, self.cards[x].y + 5 + y * self.fontSize)
 
-            if game.cards[x].isSelected:
-                drawOutlineText(game,'X', game.cards[x].x + 100,game.cards[x].y +100)
+            if self.cards[x].isSelected:
+                drawOutlineText(self,'X', self.cards[x].x + 100,self.cards[x].y +100)
 
-        drawOutlineText(game, 'Mana: ' + str(game.mana1) + '/' + str(game.totalMana1), game.SCREEN_WIDTH - 200, 50)
-        drawOutlineText(game, 'Health: ' + str(game.health1), game.SCREEN_WIDTH - 200, 50 + game.fontSize)
-        drawOutlineText(game, 'Mana: ' + str(game.mana2) + '/' + str(game.totalMana2), game.SCREEN_WIDTH - 200, game.SCREEN_HEIGHT-50)
-        drawOutlineText(game, 'Health: ' + str(game.health2), game.SCREEN_WIDTH - 200, game.SCREEN_HEIGHT-50 + game.fontSize)
-        game.screen.blit(game.passTurnButton.image, (game.passTurnButton.x, game.passTurnButton.y))
-        drawOutlineText(game, str(game.turn), game.passTurnButton.x, game.passTurnButton.y - game.fontSize)
-        drawOutlineText(game, str(game.phase), game.passTurnButton.x, game.passTurnButton.y- game.fontSize * 2)
+        drawOutlineText(self, 'Mana: ' + str(self.mana1) + '/' + str(self.totalMana1), self.SCREEN_WIDTH - 200, 50)
+        drawOutlineText(self, 'Health: ' + str(self.health1), self.SCREEN_WIDTH - 200, 50 + self.fontSize)
+        drawOutlineText(self, 'Mana: ' + str(self.mana2) + '/' + str(self.totalMana2), self.SCREEN_WIDTH - 200, self.SCREEN_HEIGHT-50)
+        drawOutlineText(self, 'Health: ' + str(self.health2), self.SCREEN_WIDTH - 200, self.SCREEN_HEIGHT-50 + self.fontSize)
+        self.screen.blit(self.passTurnButton.image, (self.passTurnButton.x, self.passTurnButton.y))
+        drawOutlineText(self, str(self.turn), self.passTurnButton.x, self.passTurnButton.y - self.fontSize)
+        drawOutlineText(self, str(self.phase), self.passTurnButton.x, self.passTurnButton.y- self.fontSize * 2)
 
 
 game = Game()
